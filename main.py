6e58ee7fbedc95541ecaf9f416c39d7fd048e24b
serder8866 <fcstefan01@gmail.com>
2025-11-25 09:52:29 +0100

    Coming back to the project after a break

--- main.py
+++ main.py
@@ -5,15 +5,18 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(type(screen))
-    print("Starting Asteroids!")
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
